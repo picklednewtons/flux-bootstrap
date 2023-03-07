d8053b61fe457b5c9e63efecdd1c81bc5bb49817
Stuart <stuart.anderson@blakyaks.com>
2023-03-07 14:38:44 +0000

    Adding KSM

--- scratch.sh
+++ scratch.sh
@@ -1,7 +1,10 @@
+# Prepare folder structure
 export CLUSTERNAME=$(ls flux/clusters/)
 mkdir "flux/apps/$CLUSTERNAME/before/namespaces" -p
 mkdir "flux/apps/$CLUSTERNAME/before/helmrepos" -p
+mkdir "flux/clusters/$CLUSTERNAME/apps" -p
 
+# Metrics namespace yaml
 cat <<EOF > "flux/apps/$CLUSTERNAME/before/namespaces/metrics.yaml"
 apiVersion: v1
 kind: Namespace
@@ -9,6 +12,7 @@ metadata:
   name: metrics
 EOF
 
+# Kustomization reference to install Namespaces
 cat <<EOF > "flux/apps/$CLUSTERNAME/before/namespaces/kustomization.yaml"
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
@@ -16,6 +20,7 @@ resources:
   - metrics.yaml
 EOF
 
+# Helm repository source for kube-state-metrics
 cat <<EOF > "flux/apps/$CLUSTERNAME/before/helmrepos/prometheus-community.yaml"
 apiVersion: source.toolkit.fluxcd.io/v1beta1
 kind: HelmRepository
@@ -27,6 +32,7 @@ spec:
   url: https://prometheus-community.github.io/helm-charts
 EOF
 
+# Kustomization reference to install Helm Repositories
 cat <<EOF > "flux/apps/$CLUSTERNAME/before/helmrepos/kustomization.yaml"
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
@@ -34,6 +40,7 @@ resources:
   - prometheus-community.yaml
 EOF
 
+# Kustomization reference to install resources required BEFORE app installation
 cat <<EOF > "flux/apps/$CLUSTERNAME/before/kustomization.yaml"
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
@@ -42,11 +49,37 @@ resources:
   - helmrepos
 EOF
 
-mkdir "flux/clusters/$CLUSTERNAME/apps" -p
-
+# Kustomization proper to install BEFORE resources
 flux create kustomization before-apps \
     --source=GitRepository/flux-system \
     --path="./flux/apps/$CLUSTERNAME/before/" \
     --prune=true \
     --interval=5m \
     --export > ./flux/clusters/$CLUSTERNAME/apps/before.yaml
+
+mkdir "flux/apps/$CLUSTERNAME/kube-state-metrics" -p
+
+# Create HlemReslease source for kube-state-metrics
+flux create hr kube-state-metrics \
+    --interval=10m \
+    --source=HelmRepository/prometheus-community \
+    --chart=kube-state-metrics \
+    --chart-version=">4.30.0" \
+    --export > ./flux/apps/$CLUSTERNAME/kube-state-metrics/kube-state-metrics.helm.yaml
+
+# Kustomization reference to install resources required BEFORE app installation
+cat <<EOF > "flux/apps/$CLUSTERNAME/kube-state-metrics/kustomization.yaml"
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - kube-state-metrics.helm.yaml
+EOF
+
+# Kustomization proper to install BEFORE resources
+flux create kustomization kube-state-metrics \
+    --source=GitRepository/flux-system \
+    --namespace=metrics \
+    --path="./flux/apps/$CLUSTERNAME/kube-state-metrics/" \
+    --prune=true \
+    --interval=5m \
+    --export > ./flux/clusters/$CLUSTERNAME/apps/kube-state-metrics.yaml
