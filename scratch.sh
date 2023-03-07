79454910f1c5f350b1b571e22cb07674a4d03f2a
Stuart <stuart.anderson@blakyaks.com>
2023-03-07 13:29:27 +0000

    Adding KSM

--- scratch.sh
+++ scratch.sh
@@ -1,5 +1,6 @@
 export CLUSTERNAME=$(ls flux/clusters/)
 mkdir "flux/apps/$CLUSTERNAME/before/namespaces" -p
+mkdir "flux/apps/$CLUSTERNAME/before/helmrepos" -p
 
 cat <<EOF >> "flux/apps/$CLUSTERNAME/before/namespaces/metrics.yaml"
 apiVersion: v1
@@ -15,11 +16,29 @@ resources:
   - metrics.yaml
 EOF
 
+cat <<EOF >> "flux/apps/$CLUSTERNAME/before/helmrepos/prometheus-community.yaml"
+apiVersion: source.toolkit.fluxcd.io/v1beta1
+kind: HelmRepository
+metadata:
+  name: prometheus-community
+spec:
+  interval: 30m
+  url: https://prometheus-community.github.io/helm-charts
+EOF
+
+cat <<EOF >> "flux/apps/$CLUSTERNAME/before/helmrepos/kustomization.yaml"
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - prometheus-community.yaml
+EOF
+
 cat <<EOF >> "flux/apps/$CLUSTERNAME/before/kustomization.yaml"
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
   - namespaces
+  - helmrepos
 EOF
 
 mkdir "flux/clusters/$CLUSTERNAME/apps" -p
